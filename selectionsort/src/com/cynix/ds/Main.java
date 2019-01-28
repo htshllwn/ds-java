b3907703c6778aa05ace280b977bc775cba34185
Hitesh Lalwani <htshllwn@gmail.com>
2019-01-28 22:12:59 +0530

    Selection Sort

---
+++ selectionsort/src/com/cynix/ds/Main.java
@@ -0,0 +1,46 @@
+package com.cynix.ds;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        System.out.println("Before Sorting: ");
+        print(intArray);
+
+        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
+            lastUnsortedIndex--) {
+            int largestIndex = 0;
+            for(int i = 1; i <= lastUnsortedIndex; i++) {
+                if(intArray[i] > intArray[largestIndex]) {
+                    largestIndex = i;
+                }
+            }
+            swap(intArray, largestIndex, lastUnsortedIndex);
+        }
+
+        System.out.println("After Sorting: ");
+        print(intArray);
+
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void print(int[] array) {
+//        System.out.println();
+        System.out.print("[ ");
+        for(int i = 0; i < array.length; i++) {
+//            System.out.print("arr[" + i + "] : " + array[i]);
+            System.out.print(array[i] + ", ");
+        }
+        System.out.print("]");
+        System.out.println();
+    }
+}
