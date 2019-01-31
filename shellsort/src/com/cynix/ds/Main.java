42384171f043e4cca59ace578b3ac2da21e8729b
Hitesh Lalwani <htshllwn@gmail.com>
2019-01-31 22:36:27 +0530

    Shell Sort

---
+++ shellsort/src/com/cynix/ds/Main.java
@@ -0,0 +1,46 @@
+package com.cynix.ds;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        System.out.println("Before Sorting: ");
+        print(intArray);
+
+        for(int gap = intArray.length / 2;
+            gap > 0;
+            gap = gap / 2) {
+
+            for(int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+
+                intArray[j] = newElement;
+            }
+        }
+
+        System.out.println("After Sorting: ");
+        print(intArray);
+
+    }
+
+
+    public static void print(int[] array) {
+//        System.out.println();
+        System.out.print("[ ");
+        for(int i = 0; i < array.length; i++) {
+//            System.out.print("arr[" + i + "] : " + array[i]);
+            System.out.print(array[i] + ", ");
+        }
+        System.out.print("]");
+        System.out.println();
+    }
+
+}
